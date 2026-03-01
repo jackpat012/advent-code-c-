7bb8dee664eeb4cd6f01b5c54a4166caa5a1b681
jackpat012 <140012724+jackpat012@users.noreply.github.com>
2026-03-01 13:55:08 -0800

    Change time output format to microseconds
    Updated time output to include microseconds count.

--- day2_2.cpp
+++ day2_2.cpp
@@ -65,7 +65,7 @@ int main() {
 	cout << "Result: " << result << endl;
 	auto end = chrono::high_resolution_clock::now();
 	auto duration = chrono::duration_cast<chrono::microseconds>(end - begin);					
-	cout << "Time: " << duration << endl;														// Executes in ~300 milliseconds on AMD 7800X3D.
+	cout << "Time: " << duration.count() << " microseconds" << endl;														// Executes in ~200 milliseconds on AMD 7800X3D Arch.
 
 	return 0;
 }
